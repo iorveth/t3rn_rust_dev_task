ebea73519cd5b45ed49f7809c1e4224e61223111
iorveth <arsen.kondratiev@protonmail.com>
2023-08-07 23:10:34 +0300

    Update logging

--- src/mock.rs
+++ src/mock.rs
@@ -3,8 +3,8 @@ use crate::{ActorAuthenticator, Config, SideEffectType, SideEffectsHandlerTrait}
 use frame_support::traits::{ConstU16, ConstU32, ConstU64, ConstU8};
 use sp_core::H256;
 use sp_runtime::{
-	traits::{BlakeTwo256, IdentityLookup},
-	BuildStorage,
+    traits::{BlakeTwo256, IdentityLookup},
+    BuildStorage,
 };
 type Block = frame_system::mocking::MockBlock<Test>;
 
@@ -14,11 +14,11 @@ pub const PARAMETERS_SPLITTER: u8 = 0x7c;
 pub const SIDE_EFFECTS_SPLITTER: u8 = 0x3b;
 
 pub const FIRST_ACCOUNT_ID: &str =
-	"5c55177d67b064bb5d189a3e1ddad9bc6646e02e64d6e308f5acbb1533ac430d";
+    "5c55177d67b064bb5d189a3e1ddad9bc6646e02e64d6e308f5acbb1533ac430d";
 pub const SECOND_ACCOUNT_ID: &str =
-	"3c55177d67b064bb5d189a3e1ddad9bc6646e02e64d6e308f5acbb1533ac430d";
+    "3c55177d67b064bb5d189a3e1ddad9bc6646e02e64d6e308f5acbb1533ac430d";
 pub const THIRD_ACCOUNT_ID: &str =
-	"9c55177d67b064bb5d189a3e1ddad9bc6646e02e64d6e308f5acbb1533ac430d";
+    "9c55177d67b064bb5d189a3e1ddad9bc6646e02e64d6e308f5acbb1533ac430d";
 
 pub const AMOUNT: u64 = 500_000;
 
@@ -30,93 +30,100 @@ pub type ActorId = <Test as Config>::ActorId;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
-	pub enum Test
-	{
-		System: frame_system,
-		TemplateModule: pallet_template,
-	}
+    pub enum Test
+    {
+        System: frame_system,
+        TemplateModule: pallet_template,
+    }
 );
 
 impl ActorAuthenticator<Test> for Test {
-	/// Ensures specific actor under given account id can commit
-	fn authorize_for_commit(
-		_: &AccountId,
-		actor_id: ActorId,
-	) -> Result<(), sp_runtime::DispatchError> {
-		if actor_id == DEFAULT_ACTOR_ID {
-			Ok(())
-		} else {
-			Err(sp_runtime::DispatchError::Other("Actor is not authorized to commit"))
-		}
-	}
+    /// Ensures specific actor under given account id can commit
+    fn authorize_for_commit(
+        _: &AccountId,
+        actor_id: ActorId,
+    ) -> Result<(), sp_runtime::DispatchError> {
+        if actor_id == DEFAULT_ACTOR_ID {
+            Ok(())
+        } else {
+            Err(sp_runtime::DispatchError::Other(
+                "Actor is not authorized to commit",
+            ))
+        }
+    }
 
-	/// Ensures specific actor under given account id can revert
-	fn authorize_for_revert(
-		_: &AccountId,
-		actor_id: ActorId,
-	) -> Result<(), sp_runtime::DispatchError> {
-		if actor_id == DEFAULT_ACTOR_ID {
-			Ok(())
-		} else {
-			Err(sp_runtime::DispatchError::Other("Actor is not authorized to revert"))
-		}
-	}
+    /// Ensures specific actor under given account id can revert
+    fn authorize_for_revert(
+        _: &AccountId,
+        actor_id: ActorId,
+    ) -> Result<(), sp_runtime::DispatchError> {
+        if actor_id == DEFAULT_ACTOR_ID {
+            Ok(())
+        } else {
+            Err(sp_runtime::DispatchError::Other(
+                "Actor is not authorized to revert",
+            ))
+        }
+    }
 }
 
 impl SideEffectsHandlerTrait for Test {
-	/// Commit given side effects
-	fn perform_commit(
-		_side_effect_type: &[SideEffectType],
-	) -> Result<(), sp_runtime::DispatchError> {
-		Ok(())
-	}
+    /// Commit given side effects
+    fn perform_commit(
+        _side_effect_type: &[SideEffectType],
+    ) -> Result<(), sp_runtime::DispatchError> {
+        Ok(())
+    }
 
-	/// Revert given side effects
-	fn perform_revert(
-		_side_effect_type: &[SideEffectType],
-	) -> Result<(), sp_runtime::DispatchError> {
-		Ok(())
-	}
+    /// Revert given side effects
+    fn perform_revert(
+        _side_effect_type: &[SideEffectType],
+    ) -> Result<(), sp_runtime::DispatchError> {
+        Ok(())
+    }
 }
 
 impl frame_system::Config for Test {
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type RuntimeOrigin = RuntimeOrigin;
-	type RuntimeCall = RuntimeCall;
-	type Nonce = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Block = Block;
-	type RuntimeEvent = RuntimeEvent;
-	type BlockHashCount = ConstU64<250>;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ConstU16<42>;
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type BaseCallFilter = frame_support::traits::Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type RuntimeOrigin = RuntimeOrigin;
+    type RuntimeCall = RuntimeCall;
+    type Nonce = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Block = Block;
+    type RuntimeEvent = RuntimeEvent;
+    type BlockHashCount = ConstU64<250>;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = ();
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ConstU16<42>;
+    type OnSetCode = ();
+    type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
 impl pallet_template::Config for Test {
-	type RuntimeEvent = RuntimeEvent;
-	type SideEffectsLengthLimit = ConstU32<SIDE_EFFECTS_LENGTH_LIMIT>;
-	type ParametersSplitter = ConstU8<PARAMETERS_SPLITTER>;
-	type SideEffectsSplitter = ConstU8<SIDE_EFFECTS_SPLITTER>;
-	type ExecutionId = u64;
-	type ActorId = u64;
+    type RuntimeEvent = RuntimeEvent;
+    type SideEffectsLengthLimit = ConstU32<SIDE_EFFECTS_LENGTH_LIMIT>;
+    type ParametersSplitter = ConstU8<PARAMETERS_SPLITTER>;
+    type SideEffectsSplitter = ConstU8<SIDE_EFFECTS_SPLITTER>;
+    type ExecutionId = u64;
+    type ActorId = u64;
 }
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	// Initialize the global logger with an env logger.
-	env_logger::init();
-	frame_system::GenesisConfig::<Test>::default().build_storage().unwrap().into()
+    // Initialize the global logger with an env logger.
+    env_logger::init();
+    frame_system::GenesisConfig::<Test>::default()
+        .build_storage()
+        .unwrap()
+        .into()
 }
--- src/types.rs
+++ src/types.rs
@@ -24,53 +24,53 @@ pub const MULTI_TRAN_EXECUTION_TYPE: u8 = 7;
 #[repr(u8)]
 #[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
 pub enum Chain {
-	Polkadot = POLKADOT_CHAIN_OR_ASSET,
-	Kusama = KUSAMA_CHAIN_OR_ASSET,
-	Rococo = ROCOCO_CHAIN_OR_ASSET,
-	T3rn = T3RN_CHAIN_OR_ASSET,
+    Polkadot = POLKADOT_CHAIN_OR_ASSET,
+    Kusama = KUSAMA_CHAIN_OR_ASSET,
+    Rococo = ROCOCO_CHAIN_OR_ASSET,
+    T3rn = T3RN_CHAIN_OR_ASSET,
 }
 
 impl Chain {
-	/// Parse `Chain` from byte, returns `None` if not supported
-	pub fn from_u8(chain_id: u8) -> Option<Self> {
-		let chain_id = match chain_id {
-			POLKADOT_CHAIN_OR_ASSET => Chain::Polkadot,
-			KUSAMA_CHAIN_OR_ASSET => Chain::Kusama,
-			ROCOCO_CHAIN_OR_ASSET => Chain::Rococo,
-			T3RN_CHAIN_OR_ASSET => Chain::T3rn,
-			_ => return None,
-		};
+    /// Parse `Chain` from byte, returns `None` if not supported
+    pub fn from_u8(chain_id: u8) -> Option<Self> {
+        let chain_id = match chain_id {
+            POLKADOT_CHAIN_OR_ASSET => Chain::Polkadot,
+            KUSAMA_CHAIN_OR_ASSET => Chain::Kusama,
+            ROCOCO_CHAIN_OR_ASSET => Chain::Rococo,
+            T3RN_CHAIN_OR_ASSET => Chain::T3rn,
+            _ => return None,
+        };
 
-		Some(chain_id)
-	}
+        Some(chain_id)
+    }
 }
 
 /// An enum representing all supported execution types and their corresponding ids
 #[repr(u8)]
 pub enum ExecutionTypes {
-	Swap = SWAP_EXEC_TYPE,
-	Tran = TRAN_EXECUTION_TYPE,
-	MultiTran = MULTI_TRAN_EXECUTION_TYPE,
+    Swap = SWAP_EXEC_TYPE,
+    Tran = TRAN_EXECUTION_TYPE,
+    MultiTran = MULTI_TRAN_EXECUTION_TYPE,
 }
 
 impl ExecutionTypes {
-	/// Parse `ExecutionTypes` from byte, returns `None` if not supported
-	pub fn from_u8(execution_type: u8) -> Option<Self> {
-		let execution_type = match execution_type {
-			SWAP_EXEC_TYPE => ExecutionTypes::Swap,
-			TRAN_EXECUTION_TYPE => ExecutionTypes::Tran,
-			MULTI_TRAN_EXECUTION_TYPE => ExecutionTypes::MultiTran,
-			_ => return None,
-		};
+    /// Parse `ExecutionTypes` from byte, returns `None` if not supported
+    pub fn from_u8(execution_type: u8) -> Option<Self> {
+        let execution_type = match execution_type {
+            SWAP_EXEC_TYPE => ExecutionTypes::Swap,
+            TRAN_EXECUTION_TYPE => ExecutionTypes::Tran,
+            MULTI_TRAN_EXECUTION_TYPE => ExecutionTypes::MultiTran,
+            _ => return None,
+        };
 
-		Some(execution_type)
-	}
+        Some(execution_type)
+    }
 }
 
 /// Represents either `Chain` or `ExecutionTypes`
 pub enum ChainOrExecutionType {
-	Chain(Chain),
-	ExecutionType(ExecutionTypes),
+    Chain(Chain),
+    ExecutionType(ExecutionTypes),
 }
 
 /// Represents `Asset` related to corresponding `Chain`
@@ -78,116 +78,138 @@ pub enum ChainOrExecutionType {
 pub struct Asset(Chain);
 
 impl Asset {
-	/// Parse `Asset` from byte, returns `None` if not supported
-	pub fn from_u8(asset_id: u8) -> Option<Self> {
-		Chain::from_u8(asset_id).map(|chain_id| Self(chain_id))
-	}
+    /// Parse `Asset` from byte, returns `None` if not supported
+    pub fn from_u8(asset_id: u8) -> Option<Self> {
+        Chain::from_u8(asset_id).map(|chain_id| Self(chain_id))
+    }
 }
 
 /// Represents all existing execution types and their corresponding values
 #[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
 pub enum ExecutionType<AccountId> {
-	Swap { caller: AccountId, to: AccountId, amount_from: Amount, amount_to: Amount, asset: Asset },
-	Tran { caller: AccountId, to: AccountId, amount: Amount },
-	MultiTran { caller: AccountId, to: AccountId, amount: Amount, asset: Asset },
+    Swap {
+        caller: AccountId,
+        to: AccountId,
+        amount_from: Amount,
+        amount_to: Amount,
+        asset: Asset,
+    },
+    Tran {
+        caller: AccountId,
+        to: AccountId,
+        amount: Amount,
+    },
+    MultiTran {
+        caller: AccountId,
+        to: AccountId,
+        amount: Amount,
+        asset: Asset,
+    },
 }
 
 /// An enum representation for side effect state
 #[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
 pub enum SideEffectState {
-	Submitted,
-	Committed,
-	Reverted,
+    Submitted,
+    Committed,
+    Reverted,
 }
 
 /// Side effect representation for `SingleChain` and `MultiChain` operations
 #[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
 pub enum SideEffectTypeRecord<AccountId> {
-	SingleChain { chain: Chain, side_effect: SideEffect<AccountId> },
-	MultiChain { chain_from: Chain, chain_to: Chain, side_effect: SideEffect<AccountId> },
+    SingleChain {
+        chain: Chain,
+        side_effect: SideEffect<AccountId>,
+    },
+    MultiChain {
+        chain_from: Chain,
+        chain_to: Chain,
+        side_effect: SideEffect<AccountId>,
+    },
 }
 
 impl<AccountId> SideEffectTypeRecord<AccountId> {
-	/// Construct new `SideEffectTypeRecord` instance with parameters provided
-	pub fn construct_side_effect(
-		chain: Chain,
-		chain_to: Option<Chain>,
-		execution_type: ExecutionType<AccountId>,
-	) -> Self {
-		if let Some(chain_to) = chain_to {
-			SideEffectTypeRecord::MultiChain {
-				chain_from: chain,
-				chain_to,
-				side_effect: SideEffect::new(execution_type),
-			}
-		} else {
-			SideEffectTypeRecord::SingleChain {
-				chain,
-				side_effect: SideEffect::new(execution_type),
-			}
-		}
-	}
+    /// Construct new `SideEffectTypeRecord` instance with parameters provided
+    pub fn construct_side_effect(
+        chain: Chain,
+        chain_to: Option<Chain>,
+        execution_type: ExecutionType<AccountId>,
+    ) -> Self {
+        if let Some(chain_to) = chain_to {
+            SideEffectTypeRecord::MultiChain {
+                chain_from: chain,
+                chain_to,
+                side_effect: SideEffect::new(execution_type),
+            }
+        } else {
+            SideEffectTypeRecord::SingleChain {
+                chain,
+                side_effect: SideEffect::new(execution_type),
+            }
+        }
+    }
 
-	/// Checks whether `SideEffect` `state` is set to `SideEffectState::Committed`
-	pub fn is_commited(&self) -> bool {
-		match self {
-			Self::SingleChain { side_effect, .. } => side_effect.is_commited(),
-			Self::MultiChain { side_effect, .. } => side_effect.is_commited(),
-		}
-	}
+    /// Checks whether `SideEffect` `state` is set to `SideEffectState::Committed`
+    pub fn is_commited(&self) -> bool {
+        match self {
+            Self::SingleChain { side_effect, .. } => side_effect.is_commited(),
+            Self::MultiChain { side_effect, .. } => side_effect.is_commited(),
+        }
+    }
 
-	/// Sets `SideEffect` `state` to `SideEffectState::Committed`
-	pub fn commit(&mut self) {
-		match self {
-			Self::SingleChain { side_effect, .. } => side_effect.commit(),
-			Self::MultiChain { side_effect, .. } => side_effect.commit(),
-		}
-	}
+    /// Sets `SideEffect` `state` to `SideEffectState::Committed`
+    pub fn commit(&mut self) {
+        match self {
+            Self::SingleChain { side_effect, .. } => side_effect.commit(),
+            Self::MultiChain { side_effect, .. } => side_effect.commit(),
+        }
+    }
 
-	/// Sets `SideEffect` `state` to `SideEffectState::Reverted`
-	pub fn revert(&mut self) {
-		match self {
-			Self::SingleChain { side_effect, .. } => side_effect.revert(),
-			Self::MultiChain { side_effect, .. } => side_effect.revert(),
-		}
-	}
+    /// Sets `SideEffect` `state` to `SideEffectState::Reverted`
+    pub fn revert(&mut self) {
+        match self {
+            Self::SingleChain { side_effect, .. } => side_effect.revert(),
+            Self::MultiChain { side_effect, .. } => side_effect.revert(),
+        }
+    }
 }
 
 /// Volatile side effect representation
 #[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
 pub struct SideEffect<AccountId> {
-	execution_type: ExecutionType<AccountId>,
-	state: SideEffectState,
+    execution_type: ExecutionType<AccountId>,
+    state: SideEffectState,
 }
 
 impl<AccountId> SideEffect<AccountId> {
-	/// Creates new instance of `SideEffect`
-	pub fn new(execution_type: ExecutionType<AccountId>) -> Self {
-		Self {
-			execution_type,
-			// for newly submitted side effects, the state is equal to `Submitted`
-			state: SideEffectState::Submitted,
-		}
-	}
+    /// Creates new instance of `SideEffect`
+    pub fn new(execution_type: ExecutionType<AccountId>) -> Self {
+        Self {
+            execution_type,
+            // for newly submitted side effects, the state is equal to `Submitted`
+            state: SideEffectState::Submitted,
+        }
+    }
 
-	/// Sets `SideEffect` `state` to `SideEffectState::Committed`
-	pub fn commit(&mut self) {
-		self.state = SideEffectState::Committed
-	}
+    /// Sets `SideEffect` `state` to `SideEffectState::Committed`
+    pub fn commit(&mut self) {
+        self.state = SideEffectState::Committed
+    }
 
-	/// Sets `SideEffect` `state` to `SideEffectState::Reverted`
-	pub fn revert(&mut self) {
-		self.state = SideEffectState::Reverted
-	}
+    /// Sets `SideEffect` `state` to `SideEffectState::Reverted`
+    pub fn revert(&mut self) {
+        self.state = SideEffectState::Reverted
+    }
 
-	/// Checks whether `SideEffect` `state` is set to `SideEffectState::Committed`
-	pub fn is_commited(&self) -> bool {
-		if let SideEffectState::Committed = self.state {
-			true
-		} else {
-			false
-		}
-	}
+    /// Checks whether `SideEffect` `state` is set to `SideEffectState::Committed`
+    pub fn is_commited(&self) -> bool {
+        if let SideEffectState::Committed = self.state {
+            true
+        } else {
+            false
+        }
+    }
 }
 
 // Aliases
@@ -196,48 +218,48 @@ pub type SideEffectsVec<T> = BoundedVec<SideEffectType, <T as Config>::SideEffec
 
 /// A numeric identifier trait
 pub trait NumericIdentifier:
-	Parameter
-	+ Member
-	+ BaseArithmetic
-	+ Codec
-	+ Default
-	+ Copy
-	+ Clone
-	+ MaybeSerializeDeserialize
-	+ Eq
-	+ PartialEq
-	+ Ord
-	+ Zero
-	+ From<u64>
-	+ Into<u64>
-	+ MaxEncodedLen
+    Parameter
+    + Member
+    + BaseArithmetic
+    + Codec
+    + Default
+    + Copy
+    + Clone
+    + MaybeSerializeDeserialize
+    + Eq
+    + PartialEq
+    + Ord
+    + Zero
+    + From<u64>
+    + Into<u64>
+    + MaxEncodedLen
 {
 }
 
 /// A trait authenticator for side effects related operations
 pub trait ActorAuthenticator<T: Config> {
-	/// Ensures specific actor under given account id can commit
-	fn authorize_for_commit(
-		account_id: &T::AccountId,
-		actor_id: T::ActorId,
-	) -> Result<(), sp_runtime::DispatchError>;
+    /// Ensures specific actor under given account id can commit
+    fn authorize_for_commit(
+        account_id: &T::AccountId,
+        actor_id: T::ActorId,
+    ) -> Result<(), sp_runtime::DispatchError>;
 
-	/// Ensures specific actor under given account id can revert
-	fn authorize_for_revert(
-		account_id: &T::AccountId,
-		actor_id: T::ActorId,
-	) -> Result<(), sp_runtime::DispatchError>;
+    /// Ensures specific actor under given account id can revert
+    fn authorize_for_revert(
+        account_id: &T::AccountId,
+        actor_id: T::ActorId,
+    ) -> Result<(), sp_runtime::DispatchError>;
 }
 
 /// A handler trait for side effects related operations
 pub trait SideEffectsHandlerTrait {
-	/// Commit given side effects
-	fn perform_commit(side_effect_type: &[SideEffectType])
-		-> Result<(), sp_runtime::DispatchError>;
-		
-	/// Revert given side effects
-	fn perform_revert(side_effect_type: &[SideEffectType])
-		-> Result<(), sp_runtime::DispatchError>;
+    /// Commit given side effects
+    fn perform_commit(side_effect_type: &[SideEffectType])
+        -> Result<(), sp_runtime::DispatchError>;
+
+    /// Revert given side effects
+    fn perform_revert(side_effect_type: &[SideEffectType])
+        -> Result<(), sp_runtime::DispatchError>;
 }
 
 impl NumericIdentifier for u64 {}
